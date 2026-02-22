63e4934086d9de959898e7a442f721f9fcaf0a19
Hey guys! Im João Ariza <168240843+JoaoAriza@users.noreply.github.com>
2026-02-22 20:45:42 -0300

    Increase axios timeout

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("DEPLOY VERSION: ", "timeout-120-v1");
+
 export const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:8080",
     timeout: 120000,
